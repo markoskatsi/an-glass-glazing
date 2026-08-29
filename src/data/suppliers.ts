4cfc56856f22535e0d06d70a8e8c0806e79ec6cd
markoskatsi <markoskatsi05@gmail.com>
2026-08-29 13:30:12 +0100

    Added some quirky css

--- src/data/suppliers.ts
+++ src/data/suppliers.ts
@@ -7,5 +7,5 @@ export const suppliers: Supplier[] = [
   { name: "Glassparts UK", url: "https://www.glassparts.co.uk/" },
   { name: "Press Glass UK", url: "https://www.pressglass.co.uk/" },
   { name: "CT Glass", url: "https://www.ctglass.co.uk" },
-  { name: "PMX Coating", url: "https://www.pmxcoating.com/" },
+  { name: "PMX Coating", url: "https://pmxcoatings.co.uk/" },
 ];
--- src/ui/Logo.tsx
+++ src/ui/Logo.tsx
@@ -2,5 +2,12 @@ import LogoImg from "../assets/logo.png";
 import "./Logo.scss";
 
 export const Logo = () => {
-  return <img src={LogoImg} alt="A&N Glass & Glazing" className="logo" />;
+  return (
+    <img
+      src={LogoImg}
+      alt="A&N Glass & Glazing"
+      className="logo"
+      onClick={() => (window.location.href = "/")}
+    />
+  );
 };
