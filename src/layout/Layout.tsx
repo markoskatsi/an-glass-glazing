0eb9adae134ac7dd952bfb846f77fb0d859b9ade
Markos Katsi <markoskatsi05@gmail.com>
2026-09-03 12:13:36 +0100

    fixed scroll issue when on about page

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -3,8 +3,17 @@ import { Footer } from "./Footer";
 import { WhyUs } from "../sections/WhyUs";
 import { Contact } from "../sections/Contact";
 import ScrollToTop from "../ui/ScrollToTop";
+import { useEffect } from "react";
 
 function Layout(props: { children: React.ReactNode }) {
+  
+  useEffect(() => {
+    const id = window.location.hash.substring(1);
+
+    document.getElementById(id)?.scrollIntoView();
+    
+  }, []);
+
   return (
     <div>
       <Header />
